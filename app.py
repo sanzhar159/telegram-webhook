24015a519437f9e4081c26f530dc5aeacf1b6c5d
sanzhar159 <sugraliev@gmail.com>
2026-03-27 22:33:52 +0500

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
-import requests
+import os
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "bot working", 200
 
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.json
     print(data)
-    return "ok"
+    return "ok", 200
 
-@app.route("/", methods=["GET"])
-def home():
-    return "bot working"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
