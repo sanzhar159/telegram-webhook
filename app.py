89b5d358642c83ac6c79679b222e9a397e6a383f
sanzhar159 <sugraliev@gmail.com>
2026-03-27 22:45:56 +0500

    app.py

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 from flask import Flask, request
+import requests
 import os
+
+TOKEN = "ТВОЙ_ТОКЕН"
+URL = f"https://api.telegram.org/bot{TOKEN}"
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return "bot working", 200
+def send_message(chat_id, text):
+    requests.post(f"{URL}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
 
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.json
-    print(data)
-    return "ok", 200
+
+    if "message" not in data:
+        return "ok"
+
+    chat_id = data["message"]["chat"]["id"]
+    text = data["message"].get("text", "")
+
+    if text == "/start":
+        send_message(chat_id,
+        "Здравствуйте! Ответьте на 3 вопроса и получите доступ к каналу\n\nКак вас зовут?")
+    else:
+        send_message(chat_id, "Спасибо! Я записал.")
+
+    return "ok"
+
+@app.route("/", methods=["GET"])
+def home():
+    return "bot working"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
